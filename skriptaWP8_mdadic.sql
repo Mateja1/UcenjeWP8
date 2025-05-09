8aa6c95fd1a8021a2088d0e9422262c948e18c52
Mateja1 <mateja.dadic@gmail.com>
2025-05-09 18:26:31 +0200

    kreiranje ostalih tablica

--- skriptaWP8_mdadic.sql
+++ skriptaWP8_mdadic.sql
@@ -3,9 +3,35 @@
 
 create table smjerovi
 	(
-	sifra int,
+	sifra int primary key,
 	naziv varchar(50),
 	cijena decimal(18,2),
 	datum_pokretanja datetime,
 	aktivan bit
 	);
+create table polaznici
+	(
+	sifra int primary key,
+	ima varchar (20),
+	prezime varchar (20),
+	email varchar (50)
+	);
+
+create table grupe
+	(
+	sifra int primary key,
+	naziv varchar (20),
+	smjer int,
+	predavac varchar (50)
+	--FOREIGN KEY (smjer_id) REFERENCES smjerovi(sifra)
+	);
+create table clan
+	(
+	grupa_id int,
+	polaznik_id varchar (20),
+	--FOREIGN KEY (grupa_id) REFERENCES grupe(sifra),
+	--FOREIGN KEY (polaznik_id) REFERENCES polaznici(sifra),
+	PRIMARY KEY (grupa_id, polaznik_id)
+	);
+
+	--drop table grupe
