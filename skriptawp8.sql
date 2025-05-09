de0823013cadcd96ae358004ab534122d2ae4b66
Mateja1 <mateja.dadic@gmail.com>
2025-05-09 18:43:29 +0200

    ispravak

---
+++ skriptawp8.sql
@@ -0,0 +1,35 @@
+﻿--create database EdunovaWP8; ---startanje za kreiranje baze
+--use EdunovaWP8;
+
+create table smjerovi
+	(
+	sifra int primary key,
+	naziv varchar(50),
+	cijena decimal(18,2),
+	datum_pokretanja datetime,
+	aktivan bit
+	);
+
+create table polaznici
+	(
+	sifra int primary key,
+	ima varchar (20),
+	prezime varchar (20),
+	email varchar (50)
+	);
+
+create table grupe
+	(
+	sifra int primary key,
+	naziv varchar (20),
+	smjer int,
+	predavac varchar (50)
+	);
+
+create table clanovi
+	(
+	grupa int,
+	polaznik varchar (20),
+	);
+
+	--drop table clan
