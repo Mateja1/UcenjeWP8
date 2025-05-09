1ec1b16d1c57bdbdb49d93667200fdfa9a90f56e
Mateja1 <mateja.dadic@gmail.com>
2025-05-09 18:07:39 +0200

    kreiranje tablice smjerovi

--- skriptaWP8_mdadic.sql
+++ skriptaWP8_mdadic.sql
@@ -1,2 +1,11 @@
 ﻿--create database EdunovaWP8; ---startanje za kreiranje baze
-use EdunovaWP8;
+--use EdunovaWP8;
+
+create table smjerovi
+	(
+	sifra int,
+	naziv varchar(50),
+	cijena decimal(18,2),
+	datum_pokretanja datetime,
+	aktivan bit
+	);
