b5b51c74fb3bd58958c36ff12e7db29db6340b65
Mateja1 <mateja.dadic@gmail.com>
2025-07-03 01:23:02 +0200

    ulaz Izlaz

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,29 @@
+﻿namespace Ucenje
+{
+    internal class E01UlazIzlaz //klasa
+    {
+
+        //metoda
+        public static void Izvedi()
+        {
+            
+            
+            Console.WriteLine("Pozdrav Suncu");
+
+            Console.WriteLine("Osijek je \n Super"); //\n je novi red
+
+            Console.WriteLine("\t\t Ovo je uvučeno");
+
+            Console.Write("Unesi svoje ime: ");
+
+            //varijabla
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste (0)", ime );
+        
+        
+        
+        
+        } 
+    }
+}//namespace
